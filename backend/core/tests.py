a59a8093948e608069992b24a0756268c474cb47
Leonardo Flores <contato@leonardocouy.com>
2016-08-15 18:29:37 -0300

    App Core has been created

---
+++ backend/core/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
+    def test_get(self):
+        """
+        GET / must return status code 200
+        """
+        self.assertEqual(200, self.resp.status_code)
