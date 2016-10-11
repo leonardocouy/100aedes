12e74fb2653249c56d092931067786db6c3033f7
Leonardo Flores <contato@leonardocouy.com>
2016-10-10 23:23:43 -0300

    Fix: Login

---
+++ backend/accounts/utils.py
@@ -0,0 +1,8 @@
+from .serializers import UserSerializer
+
+
+def _jwt_response_payload_handler(token, user=None, request=None):
+    return {
+        'token': token,
+        **UserSerializer(user).data
+    }
