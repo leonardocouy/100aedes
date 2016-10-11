bded93ab7542cb2bfc5ce0fa3f79719bd2c84d26
Leonardo Flores <contato@leonardocouy.com>
2016-10-10 23:28:16 -0300

    Feature: Rolling back Hashmap user

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -4,5 +4,5 @@ from .serializers import UserSerializer
 def _jwt_response_payload_handler(token, user=None, request=None):
     return {
         'token': token,
-        **UserSerializer(user).data
+        'user': UserSerializer(user).data
     }
