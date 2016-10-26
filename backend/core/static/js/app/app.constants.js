a5d8579e3d0d5b4f7b45d6b78e0448d2515a0f83
Leonardo Flores <contato@leonardocouy.com>
2016-10-26 13:20:56 -0200

    FIX: Constants

--- backend/core/static/js/app/app.constants.js
+++ backend/core/static/js/app/app.constants.js
@@ -5,4 +5,3 @@
         .constant('API_URL', 'https://100aedes.com.br/api/v1/reports/')
         .constant('JWT_TOKEN', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRva2VuIiwiZXhwIjoxNDc3NDMzNTI4LCJlbWFpbCI6ImNvbnRhdG9AMTAwYWVkZXMuY29tLmJyIiwidXNlcl9pZCI6NH0.L6VlbFORhdAFK5vBYXMVe52TIeWmd4pwHm7y4UFIHZg');
 })();
-
