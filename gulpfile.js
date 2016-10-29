2848ce36fff5448b4ceb2b516139f458fc5f1c11
Leonardo Flores <contato@leonardocouy.com>
2016-10-28 23:06:20 -0200

    Change name to minified file

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ gulp.task('sass', function() {
 // ['asets/js/vendorlodash/*.js', 'assets/js/vendor/jquery/*.js', 'assets/js/vendor/!(lodash, jquery)**/*.js', 'assets/js/app/*.js'
 gulp.task('scripts', function() {
   return gulp.src(['frontend/assets/js/app/app.module.js', 'frontend/assets/js/app/!(app.module)*.js','frontend/assets/js/app/home/*.js'])
-      .pipe(concat('app.js'))
+      .pipe(concat('app.min.js'))
       .pipe(ngAnnotate({add: true}))
       .pipe(uglify())
       .pipe(gulp.dest('frontend/assets/dist/'));
